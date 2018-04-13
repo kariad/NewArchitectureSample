bbc3c5ec33da96cbfa64532abb06695593104a5c
Daiki Katayama <karuiad.venezia@gmail.com>
2018-04-13 19:13:37 +0900

    Initial Commit

---
+++ NewArchitectureSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewArchitectureSample
+//
+//  Created by katayama_daiki on 2018/04/13.
+//  Copyright © 2018年 katayama_daiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
